a15f8bfd5d72c0ac9e964e71b55c6c3a9adaf0e5
NexusDevsystem <nexusdevsystem@gmail.com>
2025-05-26 00:40:54 -0300

    atualização

--- src/App.jsx
+++ src/App.jsx
@@ -20,8 +20,8 @@ export default function App() {
       <Hero />
       <About />
       <Services />
-      <SuccessCases />
       <Technologies />
+      <SuccessCases />
       <FAQ />
       <Contact />
       <Footer />
